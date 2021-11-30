9bc0fb606f0a26c6ebb1c21b41393c41b7f8f0c4
chanpapa <77497584+chanpapa@users.noreply.github.com>
2021-11-30 10:06:01 +0800

    Update run_iperf_mmdelay.sh

--- run_iperf_mmdelay.sh
+++ run_iperf_mmdelay.sh
@@ -1,5 +1,5 @@
 timeout 60 sudo tcpdump  -i 'ingress' -s 0 -w "./$1/$2-tcpdump.pcap"  &
-sudo iperf -c $MAHIMAHI_BASE -p $5 -Z $3 -t $4 -i 1 >"./$1/$2-iperf.log"
+sudo iperf3 -c $MAHIMAHI_BASE -p $5 -C $3 -t $4 -i 1 >"./$1/$2-iperf.log"
 sleep 1
 
 ~                                                                                
