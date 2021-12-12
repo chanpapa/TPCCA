3563c2ad96006b12e02e74afc244dcb6fa07a522
chanpapa <77497584+chanpapa@users.noreply.github.com>
2021-12-12 18:33:38 +0800

    Update unifomity_log_parse.py

--- unifomity_log_parse.py
+++ unifomity_log_parse.py
@@ -1,61 +1,395 @@
-import json
+#!/usr/bin/python3
+
 import os
+import subprocess
 
-import toml
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+from pathlib import Path
+from utils import tools
 
-from utils import arg_parser
-from utils.area import plot_area
-from utils.ccp_parse import bbr_parse
-from utils.uniformity_parse_tput_delay import parse_tput_delay, plot_tput_delay
-from utils.tools import makefolder
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
 
-args = arg_parser.argument_parser()
-config_file_path = args.config
-configs = toml.load(config_file_path)
+def get_throughput_data(fn):
+    cmd = "grep ' + ' {} | awk '{{print $1, $3}}'".format(fn)
+    res = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    cmd2 = "grep '# base timestamp' {} | awk '{{print $4}}'".format(fn)
+    bt = subprocess.run(cmd2, stdout=subprocess.PIPE, shell=True)
+    bt = bt.stdout.decode("utf-8").split('\n')[0]
+    for l in res.stdout.decode("utf-8").split('\n'):
+        sp = l.split(" ")
+        if len(sp) != 2:
+            continue
+        t, v = sp
+        yield float(int(t) - int(bt)) / 1e3, float(v)
 
-delay_list = configs['data']['delay_list']
-packet_buffer_list = configs['data']['packet_buffer_list']
-iteration = configs['data']['iteration']
-name_map = configs['data']['plot_name_map']
-ccp_algs = configs['data']['ccp_algs']
 
-log_folder = configs['unipath']['log_folder']
-fig_folder = configs['unipath']['fig_folder']
-trace_folder = configs['path']['trace_folder']
-ccp_fig_folder = configs['unipath']['ccp_fig_folder']
-mahimahi_fig_folder = configs['unipath']['mahimahi_fig_folder']
-area_fig_folder = configs['unipath']['area_fig_folder']
+def get_delay_data(fn):
+    cmd = "grep ' - ' {} | awk '{{print $1, $4}}'".format(fn)
+    res = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    cmd2 = "grep '# base timestamp' {} | awk '{{print $4}}'".format(fn)
+    bt = subprocess.run(cmd2, stdout=subprocess.PIPE, shell=True)
+    bt = bt.stdout.decode("utf-8").split('\n')[0]
+    for l in res.stdout.decode("utf-8").split('\n'):
+        sp = l.split(" ")
+        if len(sp) != 2:
+            continue
+        t, v = sp
+        yield float(int(t) - int(bt)) / 1e3, float(v)
 
-binsize = configs['data']['log']['binsize']
-duration = configs['data']['log']['duration']
-enable_iteration_plot = configs['data']['log']['enable_iteration_plot']
-enable_alg_plot = configs['data']['log']['enable_alg_plot']
 
-makefolder(fig_folder, ccp_fig_folder)
-makefolder(fig_folder, mahimahi_fig_folder)
-makefolder(fig_folder, area_fig_folder)
+def get_throughput(data, start_time, end_time, binsize=0):
+    bin_start = start_time
+    last_t = start_time
+    current_bin_tput = 0
+    for t, p in data:
+        if t < start_time:
+            continue
+        if t > end_time:
+            break
+        if t > (bin_start + binsize):  # start new bin
+            yield bin_start, current_bin_tput / (t - bin_start)
+            bin_start = t
+            current_bin_tput = p * 8.0
+        else:
+            current_bin_tput += p * 8.0
+            last_t = t
 
-trace_info = json.load(
-    open(os.path.join(trace_folder, 'trace_info.json'), encoding='utf-8'))
 
-print('parse bbr ccp log')
+def get_delays(data, bin_times):
+    # this function is not used but left here in case to be used
+    next_bin = next(bin_times)
+    delays = []
+    for t, p in data:
+        if t < next_bin:
+            delays.append(p)
+        else:
+            if len(delays) > 0:
+                yield np.mean(delays)
+            else:
+                yield 0
+            delays = []
+            delays.append(p)
+            next_bin = next(bin_times)
+
+    yield np.mean(delays)
+
+
+def get_times(fn, binsize):
+    bin_start = 0
+    yield 0
+    for t, _ in get_throughput_data(fn):
+        if (bin_start + binsize) > t:
+            yield t
+    yield t
+
+
+def get_expt_data(fn, duration, binsize=0):
+    tp = get_throughput_data(fn)
+    td = get_delay_data(fn)
+    ts, tp = zip(*get_throughput(tp, 0, duration, binsize))
+    ts = list(ts)
+    dl = get_delays(td, iter(ts))
+    return zip(ts, tp, dl)
+
+
+# filename: <alg>-<bw>-<bw_scenario>-<delay>-<delay_var>-mahimahi.log
+def binAlgs(fns):
+    '''
+    Parse filename.
+
+    Args:
+        fns (list): each element is a file name. <alg>-<bw>-<bw_scenario>-<delay>-<delay_var>-mahimahi.log
+
+    Returns:
+        dict: file infos
+    '''
+    plots = {}
+    for fn in fns:
+        sp = fn.split('-')
+        if sp[-1] != "mahimahi.log":
+            continue
+        alg, bw, bw_seq, packet_buf, delay, delay_var, iter_num = sp[:-1]
+        pl = (alg, bw, bw_seq, packet_buf, delay, delay_var, iter_num)
+        if pl in plots:
+            plots[pl].append(fn)
+        else:
+            plots[pl] = [fn]
+    return plots
+
+
+def parse_tput_delay(log_folder, duration, binsize=0):
+    '''
+    Parse throughput and delay. (delay is not used yet)
+
+    Args:
+        log_folder (str): folder of raw logs
+        duration (int or float): duration of time sequence
+        binsize (int, optional): binsize of throughput time interval. Defaults to 0.
+
+    Returns:
+        dict: time sequence, throughput sequence and delay sequence of log files.
+    '''
+    exps = binAlgs(sorted(os.listdir(log_folder)))
+    results = {}
+
+    print('Parse Throughput and Delay...')
+    for exp in tqdm(exps):
+        time_list = []
+        tput_list = []
+        dl_list = []
+        myfile=Path(os.path.join(log_folder, exps[exp][0]))
+        flag=0
+        if myfile.exists() :
+          flag=1          
+          for t, tput, dl in get_expt_data(
+                os.path.join(log_folder, exps[exp][0]),
+                min(duration, 2 * float(exp[4])),
+                binsize):
+            time_list.append(t)
+            tput_list.append(tput / 1000 / 1000)
+            dl_list.append(dl)
+        results[exps[exp][0]] = {
+            'flag':flag, 
+            'time_list': time_list,
+            "tput_list": tput_list,
+            "dl_list": dl_list
+        }
+        break
+    return results
+
+def restrict(a,b,c):
+  if abs(a-b)<=c:
+    return True
+  else:
+    return False
+def plot_tput_delay(ccp_algs,
+                    packet_buffer_list,
+                    trace_info,
+                    delay_list,
+                    iteration,
+                    mahimahi_results,
+                    fig_folder,
+                    enable_alg_plot=False,
+                    enable_iteration_plot=False):
+    '''
+    Plot throughput and delay(delay plot not implemented yet).
+
+    Args:
+        ccp_algs (dict): CCP algorithms dict
+        packet_buffer_list (list): packet buffer list
+        trace_info (dict): traces information
+        delay_list (list): delay list
+        iteration (int): number of iterations
+        mahimahi_results (dict): parsed mahimahi log
+        fig_folder (str): folder to save figures
+        enable_alg_plot (bool, optional): if plot every algorithm. Defaults to False.
+        enable_iteration_plot (bool, optional): if plot every iteration. Defaults to False.
+    '''
+    tools.clear_folder(fig_folder)
+    print('Plot Throughput and Delay...')
+    if enable_alg_plot:
+        for res in tqdm(mahimahi_results):
+            plt.figure(figsize=(6.4, 3))
+            plt.plot(mahimahi_results[res]['time_list'],
+                     mahimahi_results[res]['tput_list'],
+                     'ro-',
+                     label='Throughput')
+            # plt.title(res)
+            plt.xlabel('Time (s)', fontsize='12')
+            plt.ylabel('Throughput (Mbps)', fontsize='12')
+            plt.legend()
+            plt.savefig(os.path.join(fig_folder, res[:-4] + '.png'),
+                        bbox_inches='tight')
+            plt.close()
+    pbar = tqdm(total=len(packet_buffer_list) * len(trace_info) *
+                len(delay_list) * iteration)
+    for packet_buffer in packet_buffer_list:
+        for link_trace in trace_info:
+            for delay, delay_var in delay_list:
+                delay = int(delay)
+                delay_var = round(delay_var, 1)
+                plt.figure(figsize=(6.4,3))
+                count=0
+                XMAX=150
+                wnd=0.25
+                
+                for ccp_alg in ccp_algs:  
+                    tl=np.zeros((300,2), dtype=np.float)
+                    tpl=np.zeros((iteration,int(65/wnd),2,),dtype=np.float)
+                    #tp1记录相应iteration的相应区间的值
+                    #定为0.5s大小
+                    for iter_num in range(iteration):
+                        xmax=0
+                        log_name = f'{ccp_alg}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.log'
+                        
+                        try:
+                          xmax=len(mahimahi_results[log_name]['time_list'])
+                        except :
+                          continue
+                        start=0  
+                        b=link_trace[0:2]
+                        b=float(b)                     
+                        for i in range(xmax):
+                            a=int(mahimahi_results[log_name]['time_list'][i]/wnd+1)
+                            if restrict(a,10.0/wnd,(6.0/wnd)+1) or restrict(a,(20.0/wnd),(6/wnd)):
+                                tl[a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tl[a][1]+=1
+                                tpl[iter_num][a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tpl[iter_num][a][1]+=1
+                                continue
+                            if restrict(mahimahi_results[log_name]['tput_list'][i],b,15) and a< 20/wnd :
+                                tl[a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tl[a][1]+=1
+                                tpl[iter_num][a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tpl[iter_num][a][1]+=1
+                                continue
+                            if restrict(mahimahi_results[log_name]['tput_list'][i],2*b,35) and a< 40/wnd :
+                                tl[a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tl[a][1]+=1  
+                                tpl[iter_num][a][0]+=mahimahi_results[log_name]['tput_list'][i]
+                                tpl[iter_num][a][1]+=1
+                                continue
+                            
+                        '''for i in range(start,xmax):
+                          print( mahimahi_results[log_name]['time_list'][i])
+                        plt.plot(
+                            mahimahi_results[log_name]['time_list'][start:],
+                            mahimahi_results[log_name]['tput_list'][start:])
+                        pbar.update(1)
+                    # plt.title(f'{link_trace}-{delay}-{delay_var}-mahimahi')
+                    plt.xlabel('Time (s)', fontsize='12')
+                    plt.ylabel('Throughput (Mbps)', fontsize='12')
+                    plt.legend()
+                    plt.savefig(os.path.join(
+                        fig_folder,
+                        f'uniformity-{ccp_alg}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.png'
+                    ),
+                                bbox_inches='tight')
+                    plt.close()'''
+                    xl=np.zeros((600,),dtype=np.float)
+                    yl=np.zeros((600,),dtype=np.float)
+                    for i in range(int(64/wnd)):
+                        if tl[i][1] != 0:                    
+                            yl[i]=tl[i][0]/tl[i][1]
+                        for t in range(iteration):
+                            if tpl[t][i][1] !=0:
+                                tpl[t][i][0]=tpl[t][i][0]/tpl[t][i][1]              
+                    tlpercent5=[]
+                    tlpercent25=[]
+                    tlpercent75=[]
+                    tlpercent95=[]
+                    for i in range(int(63/wnd)):
+                        zq=np.zeros((iteration),dtype=np.float)
+                        for t in range(iteration):
+                            zq[t]=tpl[t][i][0]
+                        tlpercent5.append(np.percentile(zq,5))
+                        tlpercent25.append(np.percentile(zq,25))
+                        tlpercent75.append(np.percentile(zq,75)) 
+                        tlpercent95.append(np.percentile(zq,95)) 
+                    for i in range(len(tl)):
+                        xl[i]=wnd*i
+                    plt.plot(xl[0:XMAX],yl[0:XMAX] , label =f'mean')
+                    plt.plot(xl[0:XMAX],tlpercent5[0:XMAX], label=f'5%')
+                    plt.plot(xl[0:XMAX],tlpercent25[0:XMAX] , label=f'25%')
+                    plt.plot(xl[0:XMAX],tlpercent75[0:XMAX] , label=f'75%')
+                    plt.plot(xl[0:XMAX],tlpercent95[0:XMAX] , label=f'95%')
+                    plt.xlabel('Time (s)', fontsize='12')
+                    plt.ylabel('Throughput (Mbps)', fontsize='12')
+                    plt.legend()
+                    plt.savefig(os.path.join(
+                        fig_folder,
+                    f'uniformity-{ccp_alg}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.png'
+                    ),
+                    bbox_inches='tight')
+                    plt.close()
+                    pbar.update(1)
+    pbar.close()
+               #plot varience figure
 '''
-bbr_parse(packet_buffer_list, trace_info, delay_list, iteration, log_folder,
-          os.path.join(fig_folder, ccp_fig_folder))
+                fig,ax = plt.subplots(figsize=(25, 14))
+                ax.legend()
+                crh=0
+                for ccp in ccp_algs:
+                    crh+=1
+                    stat=np.zeros((350,2,150), dtype=np.float)
+                    statplot=np.array([])
+                    wnd=0.5
+                    loc=np.zeros((300,1), dtype=np.int)
+                    for iter_num in range(iteration):                   
+                        last=0
+                        log_name = f'{ccp}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.log'
+                        try:
+                            xmax=len(mahimahi_results[log_name]['time_list'])
+                            print('wrong')
+                        except :
+                            continue
+                        for i in range(xmax-10):                     
+                            a=int(mahimahi_results[log_name]['time_list'][i]/wnd+1)
+                            if not (restrict(a,10.0/wnd,(6.0/wnd)+1) or restrict(a,(20.0/wnd),(10.0/wnd))):
+                            if not(restrict(mahimahi_results[log_name]['tput_list'][i],b,12) and a< 20/wnd) :
+                            if not(restrict(mahimahi_results[log_name]['tput_list'][i],2*b,30) and a< 40/wnd) :
+                                continue
+                            stat[a][0][loc[a][0]]+=mahimahi_results[log_name]['tput_list'][i]
+                            stat[a][1][loc[a][0]]+=1
+                            if last < a:
+                                loc[a][0]+=1   #相应区间的点的数量
+                                last=a
+                        for a in range(XMAX):
+                            for i in range(loc[a][0]+1):
+                                if stat[a][1][i] !=0:
+                                stat[a][0][i]=stat[a][0][i]/stat[a][1][i]
+                                statplot=np.append(statplot,np.var(stat[a][0][0:loc[a][0]]))            
+                        xticks=np.arange(XMAX)
+                        if crh ==1 :
+                            ax.bar(xticks[40:60], statplot[40:60], width=0.25, label=f'{ccp}', color="blue")
+                        else :
+                            ax.bar(xticks[40:60]+0.25, statplot[40:60], width=0.25, label=f'{ccp}', color="red")
+                            ax.set_xticks(xticks[40:60]+0.125)
+                        xticklabels=[]
+                for i in range(XMAX):
+                  xticklabels.append(str(wnd*i)+"-"+str(wnd*i+wnd)+'s')
+                ax.set_xticklabels(xticklabels[39:59])    
+                plt.xlabel('Time (s)', fontsize='12')
+                plt.ylabel('Varience', fontsize='12')
+                plt.legend()
+                plt.savefig(os.path.join(
+                        fig_folder,
+                f'var-nonzero-{link_trace}-{packet_buffer}-{delay}-{delay_var}-{iter_num}-mahimahi.png'
+                ),
+                bbox_inches='tight')
+                plt.close()
+                      
+    pbar.close()
+    if iteration > 1 and enable_iteration_plot:
+        pbar = tqdm(total=len(packet_buffer_list) * len(trace_info) *
+                    len(delay_list) * iteration)
+        for packet_buffer in packet_buffer_list:
+            for link_trace in trace_info:
+                for delay, delay_var in delay_list:
+                    delay = int(delay)
+                    delay_var = round(delay_var, 1)
+                    for ccp_alg in ccp_algs:
+                        plt.figure(figsize=(6.4, 3))
+                        for iter_num in range(iteration):
+                            log_name = f'{ccp_alg}-{link_trace}-{delay}-{delay_var}-{iter_num}-mahimahi.log'
+                            plt.plot(
+                                mahimahi_results[log_name]['time_list'],
+                                mahimahi_results[log_name]['tput_list'],
+                                label=f'{iter_num}')
+                        # plt.title(f'{ccp_alg}-{link_trace}-{delay}-{delay_var}-mahimahi')
+                        plt.xlabel('Time (s)', fontsize='12')
+                        plt.ylabel('Throughput (Mbps)', fontsize='12')
+                        plt.legend()
+                        plt.savefig(os.path.join(
+                            fig_folder,
+                            f'iter-{ccp_alg}-{link_trace}-{packet_buffer}-{delay}-{delay_var}-mahimahi.png'
+                        ),
+                                    bbox_inches='tight')
+                        plt.close()
+                        pbar.update(1)
+        pbar.close()
 '''
-print('parse mahimahi log')
-mahimahi_results = parse_tput_delay(log_folder, duration, binsize)
-json.dump(mahimahi_results,
-          open('tmp/mahimahi_results.json', 'w', encoding='utf-8'))
-# mahimahi_results = json.load(open('tmp/mahimahi_results.json'))
-
-print('plot throughput and delay')
-plot_tput_delay(ccp_algs, packet_buffer_list, trace_info, delay_list,
-                iteration, mahimahi_results,
-                os.path.join(fig_folder, mahimahi_fig_folder), enable_alg_plot,
-                enable_iteration_plot)
-
-print('parse mahimahi log and ')
-area_dict = plot_area(ccp_algs, packet_buffer_list, trace_info, delay_list,
-                      iteration, mahimahi_results,
-                      os.path.join(fig_folder, area_fig_folder), name_map)
